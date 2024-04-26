1c191ac06701317ee2c6ace4d643c018bc1463b9
wizardyash <modi.yash18042004@gmail.com>
2024-04-26 09:27:48 +0530

    student status

--- frontend/src/pages/Student.js
+++ frontend/src/pages/Student.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useState } from 'react';
 import './Student.css'
 import Profile from './Profile'
+import Scholarship from './scholarship'
+import Previous from './Previous_Scholarship'
 const StudentPortal = () => {
     const [activeTab, setActiveTab] = useState('Profile');
 
@@ -14,9 +16,9 @@ const StudentPortal = () => {
             case 'Profile':
                 return <div className='Student-Profile'><Profile/></div>;
             case 'ScholarshipStatus':
-                return <div className='Student-Status'>Scholarship Status content goes here</div>;
+                return <div className='Student-Status'><Scholarship/></div>;
             case 'PreviousScholarships':
-                return <div className='Student-Previous'>Previous Scholarships content goes here</div>;
+                return <div className='Student-Previous'><Previous/></div>;
             default:
                 return null;
         }
@@ -26,9 +28,9 @@ const StudentPortal = () => {
         <div className='student-top'>National Institute of Technology Srinagar</div>
         <div className="student-portal-container">
             <div className="sidebar">
-                <div className={`sidebar-item ${activeTab === 'Profile' ? 'active' : ''}`} onClick={() => handleTabChange('Profile')}>
+                {/* <div className={`sidebar-item ${activeTab === 'Profile' ? 'active' : ''}`} onClick={() => handleTabChange('Profile')}>
                     Profile
-                </div>
+                </div> */}
                 <div className={`sidebar-item ${activeTab === 'ScholarshipStatus' ? 'active' : ''}`} onClick={() => handleTabChange('ScholarshipStatus')}>
                     Scholarship Status
                 </div>
