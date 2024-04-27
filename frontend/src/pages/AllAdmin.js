2f21e155650807bde4890aa21b9305daf8b69e28
Sumit Kumar <sk3700844@gmail.com>
2024-04-27 11:30:23 +0530

    updataAdminPage

--- frontend/src/pages/AllAdmin.js
+++ frontend/src/pages/AllAdmin.js
@@ -1,91 +1,138 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
+import { PiStudent } from "react-icons/pi";
+import { SiStatuspage } from "react-icons/si";
+import { CiBank } from "react-icons/ci";
+import { FaRegUserCircle } from "react-icons/fa";
+
+import { PDFDownloadLink, Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 const AllAdmin = () => {
   const [users, setUsers] = useState([]);
+  const [showTable, setShowTable] = useState(false);
 
   useEffect(()=> {
       axios.get('/getUsers')
       .then(users => setUsers(users.data))
       .catch(err => console.log(err))
-  },[])
+  },[]);
+
+  const handleShowTable = () => {
+    setShowTable(true);
+  };
+  const handleDownloadPDF = () => {
+    // Select the element which you want to convert to pdf
+    const input = document.getElementById('pdf-table');
+
+    // Create a canvas
+    html2canvas(input)
+      .then((canvas) => {
+        const imgData = canvas.toDataURL('image/png');
+        const pdf = new jsPDF();
+        const imgWidth = 210;
+        const imgHeight = canvas.height * imgWidth / canvas.width;
+        pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
+        pdf.save("download.pdf");
+      });
+  };
+  
 
   return (
     <>
       <div className='admin-container'>
         <div className='admin-top'>Scholarship Entry Page</div>
-        <div className="admin-content">
-          <div className='admin-content-1'>
-            <label htmlFor="session"><span>*</span>Session</label>
-            <select  className='session-Drop-box drop-box'>
-              <option value="session">SPRING 2024</option>
-              <option value="session">AUTUMN 2024</option>
-            </select>
-            <label htmlFor="year"><span>*</span>Year</label>
-            <select  className='year-Drop-box drop-box'>
-              <option value="student">2024</option>
-              <option value="admin">2023</option>
-            </select>
-            <label htmlFor="month"><span>*</span>Month</label>
-            <select  className='month-Drop-box drop-box'>
-              <option value="student">April</option>
-              <option value="admin">March</option>
-            </select>
+        <div className="admin-container-content">
+          <div className="admin-container-content-1">
+            <div className="admin-sidebar">
+              <div className="admin-sidebar-content">
+                <div className="current-admin line"><FaRegUserCircle  className='react-icon' /> #Sumit</div> 
+                <div className="line"><PiStudent /> Student Details</div>
+                <div className="line"><SiStatuspage /> Status</div>
+                <div className="line"><CiBank /> Bank's Details</div>
+              </div>
+            </div>
           </div>
-          <div className='admin-content-2'>
-            <label htmlFor="degree"><span>*</span>Degree</label>
-            <select  className='degree-Drop-box drop-box'>
-              <option value="student">PhD</option>
-            </select>
-            <label htmlFor="branch"><span>*</span>Branch</label>
-            <select  className='branch-Drop-box drop-box'>
-              <option value="student">Computer Science Engineering</option>            </select>
+          <div className="admin-container-content-2">
+            <div className="admin-content">
+            <div className='admin-content-1'>
+              <label htmlFor="session"><span>*</span>Session</label>
+              <select  className='session-Drop-box drop-box'>
+                <option value="session">SPRING 2024</option>
+                <option value="session">AUTUMN 2024</option>
+              </select>
+              <label htmlFor="year"><span>*</span>Year</label>
+              <select  className='year-Drop-box drop-box'>
+                <option value="student">2024</option>
+                <option value="admin">2023</option>
+              </select>
+              <label htmlFor="month"><span>*</span>Month</label>
+              <select  className='month-Drop-box drop-box'>
+                <option value="student">April</option>
+                <option value="admin">March</option>
+              </select>
+            </div>
+            <div className='admin-content-2'>
+              <label htmlFor="degree"><span>*</span>Degree</label>
+              <select  className='degree-Drop-box drop-box'>
+                <option value="student">PhD</option>
+              </select>
+              <label htmlFor="branch"><span>*</span>Branch</label>
+              <select  className='branch-Drop-box drop-box'>
+                <option value="student">Computer Science Engineering</option>            </select>
+            </div>
           </div>
+          <div className="admin-buttons">
+            <button className='btn' onClick={handleShowTable}>Show</button>
+            <button className='btn'>Excel Report</button>
+            <button className='btn' onClick={handleDownloadPDF}>Pdf Report</button>
+          </div>
+          </div>
+          {/* <div className="scholarship-detail">Scholarship Details PhD</div> */}
         </div>
-        <div className="admin-buttons">
-          <button className='btn'>Show</button>
-          <button className='btn'>Excel Report</button>
-          <button className='btn'>Pdf Report</button>
-        </div>
-        <div className="scholarship-detail">Scholarship Details PhD</div>
-        <table>
-        <thead>
-            <tr>
-              <th>Reg No-Name</th>
-              <th>Branch</th>
-              <th>Semester</th>
-              <th>Bank A/C</th>
-              <th>Full</th>
-              <th>Total Days</th>
-              <th>Entitlement</th>
-              <th>Actual Scholarship</th>
-              <th>HRA @18% of Scholarship</th>
-              <th>Net Amount</th>
-              <th>Supervisor</th>
-            </tr>
-        </thead>
-        <tbody>
-          {
-            users.map(user => {
-              return (<tr>
-                <td>{user.enrollment}-{user.username}</td>
-                <td>CSE</td>
-                <td>IV</td>
-                <td>xyz</td>
-                <td><input type="checkbox" className='checkbox' /></td>
-                <td><input type="number" /></td>
-                <td><input type="number" /></td>
-                <td><input type="number" /></td>
-                <td><input type="number" /></td>
-                <td><input type="number" /></td>
-                <td>Sparsh Sharma</td>
-              </tr>
-              )
-            })
-          }
-        </tbody>
-      </table>
       </div>
+      {showTable&& (
+        <div id="pdf-table">
+          <table>
+            <thead>
+                <tr>
+                  <th>Reg No-Name</th>
+                  <th>Branch</th>
+                  <th>Semester</th>
+                  <th>Bank A/C</th>
+                  <th>Full</th>
+                  <th>Total Days</th>
+                  <th>Entitlement</th>
+                  <th>Actual Scholarship</th>
+                  <th>HRA @18% of Scholarship</th>
+                  <th>Net Amount</th>
+                  <th>Supervisor</th>
+                </tr>
+            </thead>
+            <tbody>
+              {
+                users.map(user => {
+                  return (<tr>
+                    <td>{user.enrollment}-{user.name}</td>
+                    <td>CSE</td>
+                    <td>IV</td>
+                    <td>xyz</td>
+                    <td><input type="checkbox" className='checkbox' /></td>
+                    <td><input type="number" /></td>
+                    <td><input type="number" /></td>
+                    <td><input type="number" /></td>
+                    <td><input type="number" /></td>
+                    <td><input type="number" /></td>
+                    <td>Sparsh Sharma</td>
+                  </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table> 
+        </div>
+      )}
     </>
   )
 }
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,35 +1,54 @@
 import React, { useState } from 'react';
 import logo from '../image/logo.png';
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
-export default function Register() {
-    const [role, setRole] = useState('student');
-    const [name, setName] = useState('');
-    const [enrollment, setEnrollment] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+const Register = () => {
+    const [user, setUser] = useState({
+        name: "",
+        enrollment: "",
+        username: "",
+        password: "",
+    });
 
-    const RoleChange= (event) => {
-        setRole(event.target.value);
+    const navigate = useNavigate();
+    const URL = "http://localhost:5000/api/auth/register";
+
+    const handleInput = (e) => {
+        let name = e.target.name;
+        let value = e.target.value;
+
+        setUser({
+            ...user,
+            [name]: value,
+        });
     };
 
-    const NameChange= (event) => {
-        setName(event.target.value);
-    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        try {
+            const response = await fetch(URL, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(user),
+            });
 
-    const EnrollmentChange= (event) => {
-        setEnrollment(event.target.value);
-    };
+            const responseData = await response.json();
 
-    const UsernameChange = (event) => {
-        setUsername(event.target.value);
-    };
+            if(response.ok) {
+                setUser({name: "", enrollment: "", username: "", password: ""});
+                navigate("/");
+            } else {
+                toast.error(responseData.extraDetails ? responseData.extraDetails: responseData.message);
 
-    const PasswordChange = (event) => {
-        setPassword(event.target.value);
-    };
-
-    const handleSubmit = () => {
-    };
+            }
+        } catch (error) {
+            console.log("signup error", error);
+        }
+    }
 
     return (
         <div className='Register-container'>
@@ -38,34 +57,58 @@ export default function Register() {
                 <div className='Register-container-2'>
                     <img src={logo} alt="Logo" />
                     <h1>Registration Form</h1>
-                    <div className='Register-container-3'>
-                        <div className="input-group">
-                            <label htmlFor="role">Role</label>
-                            <select id="role" value={role} onChange={RoleChange} className='Register-Drop-box'>
-                                <option value="student">Student</option>
-                                <option value="admin">Admin</option>
-                            </select>
-                        </div>
-                        <div className="input-group">
-                            <label htmlFor="name">Name</label>
-                            <input type='text' id="name" value={name} onChange={NameChange} />
-                        </div>
-                        <div className="input-group">
-                            <label htmlFor="enrollment">Enrollment</label>
-                            <input type='text' id="enrollment" value={enrollment} onChange={EnrollmentChange} />
-                        </div>
-                        <div className="input-group">
-                            <label htmlFor="username">Username</label>
-                            <input type='text' id="username" value={username} onChange={UsernameChange} />
-                        </div>
-                        <div className="input-group">
-                            <label htmlFor="password">Password</label>
-                            <input type='password' id="password" value={password} onChange={PasswordChange} />
-                        </div>
-                    </div>
-                    <button type='submit' onClick={handleSubmit} className='submit'>Submit</button>
+                    <form onSubmit={handleSubmit}>
+                        <div className='Register-container-3'>
+                            <div className="input-group">
+                                <label htmlFor="role">Role</label>
+                                <select id="role" className='Register-Drop-box'>
+                                    <option value="student">Student</option>
+                                    <option value="admin">Admin</option>
+                                </select>
+                            </div>
+                            <div className="input-group">
+                                <label htmlFor="name">Name</label>
+                                <input 
+                                    type='text' 
+                                    id="name" 
+                                    value={user.name}
+                                    onChange={(e)=>setUser(e.target.value)}
+                                />
+                            </div>
+                            <div className="input-group">
+                                <label htmlFor="enrollment">Enrollment</label>
+                                <input 
+                                    type='text' 
+                                    id="enrollment" 
+                                    value={user.enrollment}
+                                    onChange={handleInput}
+                                />
+                            </div>
+                            <div className="input-group">
+                                <label htmlFor="username">Username</label>
+                                <input 
+                                    type='text' 
+                                    id="username"
+                                    value={user.username}
+                                    onChange={handleInput}
+                                />
+                            </div>
+                            <div className="input-group">
+                                <label htmlFor="password">Password</label>
+                                <input 
+                                    type='password' 
+                                    id="password"
+                                    value={user.password}
+                                    onChange={handleInput}
+                                />
+                            </div>
+                        </div>                 
+                        <button type='submit' className='submit'>Submit</button>
+                    </form>
                 </div>
             </div>
         </div>
     );
 }
+
+export default Register;
